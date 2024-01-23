b9b9f0a5da3633666e43bfff288d76091c006c43
vishal <parmarvishal1942@gmail.com>
2024-01-23 20:19:12 +0530

    design of page 3 done

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+let elements = document.querySelectorAll(".first-line");
+
 let container = document.querySelector(".line-container");
 let fixed = document.querySelector(".fixed-img");
 
 
-container.addEventListener("mouseenter",(e)=>{
+container.addEventListener("mouseenter",()=>{
 fixed.style.display = "block";
-fixed.classList.add("fixed-bgimage");
-console.log(e);
+
 });
 container.addEventListener("mouseleave",()=>{
     fixed.style.display = "none";
 
     });
+elements.forEach((ele)=>{
+    ele.addEventListener("mouseenter",()=>{
+        var image1 = ele.getAttribute("src");
+        fixed.style.backgroundImage = `url(${image1})`;
+        fixed.classList.add("fixed-bgimage");
+        })
+       
+})
