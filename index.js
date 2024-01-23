ef372b72d63232a3214757082683046aa187110e
vishal <parmarvishal1942@gmail.com>
2024-01-23 14:08:57 +0530

    added on hover effect

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+let container = document.querySelector(".line-container");
+let fixed = document.querySelector(".fixed-img");
+
+
+container.addEventListener("mouseenter",(e)=>{
+fixed.style.display = "block";
+fixed.classList.add("fixed-bgimage");
+console.log(e);
+});
+container.addEventListener("mouseleave",()=>{
+    fixed.style.display = "none";
+
+    });
