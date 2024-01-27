c7493557f9e58f4b71ac54bb9194e50e613c1b7e
vishal <parmarvishal1942@gmail.com>
2024-01-27 17:00:30 +0530

    site completed,might add some more animation

--- index.js
+++ index.js
@@ -9,19 +9,31 @@ let container = document.querySelector(".line-container");
 let fixed = document.querySelector(".fixed-img");
 
 
-container.addEventListener("mouseenter",()=>{
-fixed.style.display = "block";
-
-});
-container.addEventListener("mouseleave",()=>{
-    fixed.style.display = "none";
-
-    });
-elements.forEach((ele)=>{
-    ele.addEventListener("mouseenter",()=>{
-        var image1 = ele.getAttribute("src");
-        fixed.style.backgroundImage = `url(${image1})`;
-        fixed.classList.add("fixed-bgimage");
+function page3 (){
+    container.addEventListener("mouseenter",()=>{
+        fixed.style.display = "block";
+        
+        });
+        container.addEventListener("mouseleave",()=>{
+            fixed.style.display = "none";
+        
+            });
+        elements.forEach((ele)=>{
+            ele.addEventListener("mouseenter",()=>{
+                var image1 = ele.getAttribute("src");
+                fixed.style.backgroundImage = `url(${image1})`;
+                fixed.classList.add("fixed-bgimage");
+                })
+               
         })
-       
-})
+}
+function swiper1(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 100,
+        freeMode: true,
+            
+      });
+}
+page3();
+swiper1();
